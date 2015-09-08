777d914d4ab874ed2d2ba491fcf746050470c092
tilman <kontakt@tilman-ginzel.de>
2015-09-08 16:24:34 +0200

    Added todo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\University;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-
+use Flynsarmy\CsvSeeder\CsvSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,24 +15,42 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        $this->call(HsrmSeeder::class);
+        $this->call(HochschulkompassSeeder::class);
 
         Model::reguard();
     }
 }
 
-
-class HsrmSeeder extends Seeder
+class HochschulkompassSeeder extends CsvSeeder
 {
     /**
-     * Run the database seeds.
-     *
-     * @return void
+     * Configure the csv seeder.
+     */
+    public function __construct()
+    {
+        $this->table = 'universities';
+        $this->filename = base_path().'/database/seeds/hochschulkompass-alle-hochschulen.tsv'; // TODO: update path?
+        $this->csv_delimiter = "\t";
+        $this->offset_rows = 1;
+        $this->mapping = [
+            1 => "short_name",
+            2 => "name",
+            4 => "sponsorship",
+            5 => "state",
+            6 => "student_count",
+            7 => "year_established",
+            10 => "street",
+            11 => "plz",
+            12 => "city",
+            19 => "website"
+        ];
+    }
+
+    /**
+     * Run the hochschulkompass seeder.
      */
     public function run()
     {
-        $hsrm = University::create([
-            "name" => "Hochschule RheinMain"
-        ]);
+        parent::run();
     }
 }
