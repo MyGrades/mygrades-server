a4664b56ba29a4bc9f2888471298635fada22a8b
jonastheis <mail@jonastheis.de>
2015-09-11 19:43:26 +0200

    - tests updated

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\University;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -15,70 +16,36 @@ class ApiTest extends TestCase
      */
     public function testGetUniversities()
     {
-        $this->get('/universities')
-            ->seeJsonEquals([
-                'created' => true,
-            ]);
+        $this->get($this->apiPrefix . '/universities')
+            ->seeJsonEquals(University::all()->toArray());
     }
 
     public function testGetSingleUniversity()
-    {
-        /*
-         * {
-  "university_id": 333,
-  "short_name": "RheinMain H",
-  "name": "Hochschule RheinMain, RheinMain University of Applied Sciences Wiesbaden, Rüsselsheim",
-  "sponsorship": "staatlich",
-  "state": "Hessen",
-  "student_count": 11407,
-  "year_established": 1971,
-  "street": "Kurt-Schumacher-Ring 18",
-  "plz": "65197",
-  "city": "Wiesbaden",
-  "website": "http://www.hs-rm.de",
-  "created_at": "-0001-11-30 00:00:00",
-  "updated_at": "-0001-11-30 00:00:00",
-  "rules": [
-    {
-      "rule_id": 1,
-      "type": "bachelor",
-      "university_id": 333,
-      "created_at": "2015-09-09 21:59:01",
-      "updated_at": "2015-09-09 21:59:01",
-      "actions": []
-    },
-    {
-      "rule_id": 2,
-      "type": "master",
-      "university_id": 333,
-      "created_at": "2015-09-09 21:59:01",
-      "updated_at": "2015-09-09 21:59:01",
-      "actions": []
-    },
-    {
-      "rule_id": 3,
-      "type": "registeredTests",
-      "university_id": 333,
-      "created_at": "2015-09-09 21:59:01",
-      "updated_at": "2015-09-09 21:59:01",
-      "actions": []
-    }
-  ]
-}
-         */
-
-        $this->get('/university/333')
+        {
+        $this->get($this->apiPrefix . '/university/333')
             ->seeJsonEquals([
-                "rule_id" => true,
-
+                "university_id" => 333,
+                "short_name" => "RheinMain H",
+                "name" => "Hochschule RheinMain, RheinMain University of Applied Sciences Wiesbaden, Rüsselsheim",
+                "sponsorship" => "staatlich",
+                "state" => "Hessen",
+                "student_count" => 11407,
+                "year_established" => 1971,
+                "street" => "Kurt-Schumacher-Ring 18",
+                "plz" => "65197",
+                "city" => "Wiesbaden",
+                "website" => "http://www.hs-rm.de",
+                "created_at" => "-0001-11-30 00:00:00",
+                "updated_at" => "-0001-11-30 00:00:00"
             ]);
     }
 
+/*
     public function testGetSingleUniversityDetailed()
     {
         $this->post('/user', ['name' => 'Sally'])
             ->seeJsonEquals([
-                'created' => true,
+                'created' => true
             ]);
     }
 
@@ -91,7 +58,7 @@ class ApiTest extends TestCase
     {
         $this->post('/user', ['name' => 'Sally'])
             ->seeJsonEquals([
-                'created' => true,
+                'created' => true
             ]);
-    }
+    }*/
 }
