40dd1a93f883ace83669a38fc4199b920332de20
Jonas <asd>
2015-09-20 13:18:31 +0200

    Updated Models with PHPdoc.

--- app/Action.php
+++ app/Action.php
@@ -1,5 +1,28 @@
 <?php
-
+/**
+ * App\Action
+ *
+ * @property integer $action_id
+ * @property integer $position
+ * @property string $method
+ * @property string $url
+ * @property string $parse_expression
+ * @property string $parse_type
+ * @property integer $rule_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Rule $rule
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\ActionParam[] $actionParams
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereActionId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action wherePosition($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereMethod($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereUrl($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereParseExpression($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereParseType($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereRuleId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Action whereUpdatedAt($value)
+ */
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
--- app/University.php
+++ app/University.php
@@ -3,7 +3,39 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * App\University
+ *
+ * @property integer $university_id
+ * @property boolean $published
+ * @property string $short_name
+ * @property string $name
+ * @property string $sponsorship
+ * @property string $state
+ * @property integer $student_count
+ * @property integer $year_established
+ * @property string $street
+ * @property string $plz
+ * @property string $city
+ * @property string $website
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Rule[] $rules
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereUniversityId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University wherePublished($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereShortName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereSponsorship($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereState($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereStudentCount($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereYearEstablished($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereStreet($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University wherePlz($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereCity($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereWebsite($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\University whereUpdatedAt($value)
+ */
 class University extends Model
 {
     /**
