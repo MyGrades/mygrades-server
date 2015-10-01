75a7c00325dffd59b40252390cfe902296aaced0
tilman <kontakt@tilman-ginzel.de>
2015-10-01 10:36:02 +0200

    Added unit test to check published universities

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -10,57 +10,18 @@ class ApiTest extends TestCase
     protected $apiPrefix = "/api/v1";
 
     /**
-     * A basic functional test example.
-     *
-     * @return void
+     * Test if all published universities have at least one rule with at least one action.
      */
-    public function testGetUniversities()
+    public function testPublishedUniversities()
     {
-        /*$this->get($this->apiPrefix . '/universities')
-            ->seeJsonEquals(University::all()->toArray());*/
-        $this->assertTrue(true);
+        $universities = University::published(true)->get();
+        foreach($universities as $university)
+        {
+            $this->assertTrue($university->published === true);
+            $this->assertGreaterThan(0, $university->rules->count());
+            foreach($university->rules as $rule) {
+                $this->assertGreaterThan(0, $rule->actions->count());
+            }
+        }
     }
-
-    public function testGetSingleUniversity()
-    {
-        /*$this->get($this->apiPrefix . '/university/333')
-            ->seeJsonEquals([
-                "university_id" => 333,
-                "short_name" => "RheinMain H",
-                "name" => "Hochschule RheinMain, RheinMain University of Applied Sciences Wiesbaden, Rüsselsheim",
-                "sponsorship" => "staatlich",
-                "state" => "Hessen",
-                "student_count" => 11407,
-                "year_established" => 1971,
-                "street" => "Kurt-Schumacher-Ring 18",
-                "plz" => "65197",
-                "city" => "Wiesbaden",
-                "website" => "http://www.hs-rm.de",
-                "created_at" => "-0001-11-30 00:00:00",
-                "updated_at" => "-0001-11-30 00:00:00"
-            ]);*/
-        $this->assertTrue(true);
-    }
-
-/*
-    public function testGetSingleUniversityDetailed()
-    {
-        $this->post('/user', ['name' => 'Sally'])
-            ->seeJsonEquals([
-                'created' => true
-            ]);
-    }
-
-    public function testCreateWishForUniversity()
-    {
-
-    }
-
-    public function testCreateErrorForUniversity()
-    {
-        $this->post('/user', ['name' => 'Sally'])
-            ->seeJsonEquals([
-                'created' => true
-            ]);
-    }*/
 }
