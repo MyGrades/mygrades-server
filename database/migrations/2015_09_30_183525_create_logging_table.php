e096eef40317b5e50f9476cbb11e3752c50d7aff
jonastheis <mail@jonastheis.de>
2015-10-03 10:23:02 +0200

    changed logging to utc time.

--- database/migrations/2015_09_30_183525_create_logging_table.php
+++ database/migrations/2015_09_30_183525_create_logging_table.php
@@ -21,7 +21,7 @@ class CreateLoggingTable extends Migration
 
             $table->integer('response_code');
 
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at');
         });
     }
 
