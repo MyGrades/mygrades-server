78e1157781931c58780d6dfa9cfeef450322bd14
tilman <kontakt@tilman-ginzel.de>
2015-10-13 14:47:27 +0200

    Updated unit tests to test all (detailed) published universities

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -10,38 +10,60 @@ class ApiTest extends TestCase
     protected $apiPrefix = "/api/v1";
 
     /**
-     * Test if all published universities have at least one rule with at least one action.
+     * Test if all published universities have the necessary attributes.
      */
     public function testPublishedUniversities()
     {
-        University::published(true)->get()->each(function ($university)
-        {
-            $this->assertTrue($university->published === true);
-            $this->assertGreaterThan(0, $university->rules->count());
+        // get all published universities
+        $response = $this->call('GET', $this->apiPrefix . '/universities?published=true');
 
-            $university->rules->each(function ($rule) {
-                $this->assertGreaterThan(0, $rule->actions->count());
-            });
-        });
+        $universities = $this->parseJson($response);
+        foreach($universities as $university)
+        {
+            $this->assertObjectHasAttribute("university_id", $university);
+            $this->assertObjectHasAttribute("published", $university);
+            $this->assertObjectHasAttribute("name", $university);
+            $this->assertObjectHasAttribute("updated_at_server", $university);
+            $this->assertEquals($university->published, true);
+        }
     }
 
     /**
-     * TODO: call function on each published university
+     * Test all published universities and its details.
      */
-    public function testSingleUniversityDetailed()
+    public function testPublishedUniversitiesWithDetails()
     {
-        $response = $this->call('GET', $this->apiPrefix . '/universities/333?detailed=true');
-        $data = $this->parseJson($response);
+        // get all published universities
+        $response = $this->call('GET', $this->apiPrefix . '/universities?published=true');
 
-        $this->assertObjectHasAttribute("university_id", $data);
-        $this->assertObjectHasAttribute("published", $data);
-        $this->assertObjectHasAttribute("name", $data);
-        $this->assertObjectHasAttribute("updated_at_server", $data);
+        $universities = $this->parseJson($response);
+        foreach($universities as $university)
+        {
+            // get detailed university
+            $detailedResponse = $this->call('GET', $this->apiPrefix . '/universities/' . $university->university_id . '?detailed=true');
+            $detailedUniversity = $this->parseJson($detailedResponse);
+            $this->checkSingleUniversityDetailed($detailedUniversity);
+        }
+    }
+
+    /**
+     * Checks if the given university has all necessary attributes.
+     *
+     * @param $university
+     */
+    public function checkSingleUniversityDetailed($university)
+    {
+        $this->assertObjectHasAttribute("university_id", $university);
+        $this->assertObjectHasAttribute("published", $university);
+        $this->assertObjectHasAttribute("name", $university);
+        $this->assertObjectHasAttribute("updated_at_server", $university);
 
         // check structure of rules
-        $this->assertObjectHasAttribute("rules", $data);
-        $this->assertGreaterThan(0, count($data->rules));
-        foreach ($data->rules as $rule) {
+        $this->assertObjectHasAttribute("rules", $university);
+        $this->assertGreaterThan(0, count($university->rules));
+        foreach ($university->rules as $rule)
+        {
+            var_dump($rule);
             // set booleans for action params username and password
             $usernameIsPresent = false;
             $passwordIsPresent = false;
@@ -53,11 +75,13 @@ class ApiTest extends TestCase
             $this->assertObjectHasAttribute("semester_start_summer", $rule);
             $this->assertObjectHasAttribute("semester_start_winter", $rule);
             $this->assertObjectHasAttribute("grade_factor", $rule);
+            $this->assertObjectHasAttribute("overview", $rule);
 
             // check structure of actions
             $this->assertObjectHasAttribute("actions", $rule);
             $this->assertGreaterThan(0, count($rule->actions));
-            foreach ($rule->actions as $action) {
+            foreach ($rule->actions as $action)
+            {
                 $this->assertObjectHasAttribute("action_id", $action);
                 $this->assertObjectHasAttribute("position", $action);
                 $this->assertObjectHasAttribute("method", $action);
@@ -66,7 +90,8 @@ class ApiTest extends TestCase
                 $this->assertObjectHasAttribute("action_params", $action);
 
                 // check structure of action_params if present
-                if (count($action->action_params) > 0) {
+                if (count($action->action_params) > 0)
+                {
                     foreach ($action->action_params as $action_param) {
                         $this->assertObjectHasAttribute("action_param_id", $action_param);
                         $this->assertObjectHasAttribute("key", $action_param);
@@ -86,7 +111,8 @@ class ApiTest extends TestCase
             // check structure of transformer_mappings
             $this->assertObjectHasAttribute("transformer_mappings", $rule);
             $this->assertGreaterThan(0, count($rule->transformer_mappings));
-            foreach ($rule->transformer_mappings as $transformer_mapping) {
+            foreach ($rule->transformer_mappings as $transformer_mapping)
+            {
                 $this->assertObjectHasAttribute("transformer_mapping_id", $transformer_mapping);
                 $this->assertObjectHasAttribute("name", $transformer_mapping);
                 $this->assertObjectHasAttribute("parse_expression", $transformer_mapping);
@@ -95,11 +121,31 @@ class ApiTest extends TestCase
             // make sure that username and password is present in rule
             $this->assertTrue($usernameIsPresent, "Username is present");
             $this->assertTrue($passwordIsPresent, "Password is present");
+
+            // if overview == true, check for actions with type = table_overview
+            if ($rule->overview === true)
+            {
+                $tableOverviewIsPresent = false;
+                foreach ($rule->actions as $action)
+                {
+                    if ($action->type === 'table_overview')
+                    {
+                        $tableOverviewIsPresent = true;
+                        break;
+                    }
+                }
+                $this->assertEquals(true, $tableOverviewIsPresent);
+            }
         }
     }
 
 
-
+    /**
+     * Parse json to a php object.
+     *
+     * @param $response
+     * @return mixed
+     */
     private function parseJson($response)
     {
         return json_decode($response->getContent());
