c8cadb5e3720e37acf3904c49fe8649f3ec4ce42
jonastheis <mail@jonastheis.de>
2015-12-11 16:42:39 +0100

    implemented scheduler for checking error entries in DB and sending mail to us.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +27,28 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+       // TODO: test functionality on live system and change frequency
+
+        // check if there is a new error
+        $schedule->call(function () {
+            // get new errors
+            $new_errors = DB::table('errors')
+                ->whereNull('cron_seen')
+                ->get();
+            if ($new_errors !== null && count($new_errors) > 0) {
+
+                Mail::raw('Text to e-mail \n ' . json_encode($new_errors), function ($message) {
+                    $message->to("hallo@mygrades.de", $name = null);
+                    $message->subject("New errors reported");
+                });
+
+                echo json_encode($new_errors);
+
+                // mark them as seen
+                DB::table('errors')
+                    ->whereNull('cron_seen')
+                    ->update(['cron_seen' => Carbon::now()]);
+            }
+        })->everyMinute();
     }
 }
