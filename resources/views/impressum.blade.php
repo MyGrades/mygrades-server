bfcc6ae55816a0b3ba201e1f715dfa83e24525f6
jonastheis <mail@jonastheis.de>
2016-01-02 19:04:04 +0100

    adapted header in legal notice.

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -23,7 +23,7 @@
         <p> </p>
 
 
-        <h1>Haftungsausschluss (Disclaimer)</h1>
+        <h2>Haftungsausschluss (Disclaimer)</h2>
         <p><strong>Haftung für Inhalte</strong><br/>
         Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
         <p><strong>Haftung für Links</strong><br/>
